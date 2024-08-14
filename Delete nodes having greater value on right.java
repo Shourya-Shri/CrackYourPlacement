7ebbea17581276d86243f9d63c170a84ea900148
Shourya Shri <112325367+Shourya-Shri@users.noreply.github.com>
2024-08-14 23:20:22 +0530

    Create Delete nodes having greater value on right.java

---
+++ Delete nodes having greater value on right.java
@@ -0,0 +1,29 @@
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node temp)
+    {
+        if(temp.next==null){
+            return temp;
+        }
+        Node connect=compute(temp.next);
+        
+        if(temp.data>=connect.data){
+            temp.next=connect;
+            return temp;
+        }
+        
+        return connect;
+      
+    }
+    
+}
